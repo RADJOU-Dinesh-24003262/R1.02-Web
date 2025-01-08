e01a84a86d706493b344567b15cd2ba2ceb9b090
Dinesh <dinesh.radjou@laposte.net>
2025-01-08 11:53:37 +0100

    test sombre reste

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,23 @@
 function changerStyle(css) {
-    var linkElement = document.getElementById('style');
-    var currentStyle = linkElement.getAttribute('href');
-    
-    if (currentStyle.includes("alternative")) {
-      linkElement.setAttribute('href', "style/"+ css);
-    }else{
-      linkElement.setAttribute('href', "style/alternative/" + css)
-    }
-      
+  var linkElement = document.getElementById('style');
+  var currentStyle = linkElement.getAttribute('href');
+  let styleValue = localStorage.getItem("style");
+
+  console.log(styleValue);
+  if (styleValue === null) {
+    localStorage.setItem("style", "normal");
+    linkElement.setAttribute('href', "style/" + css);
+    console.log(styleValue);
   }
+  else if(styleValue == "normal"){
+    linkElement.setAttribute('href', "style/alternative/"+ css);
+    localStorage.setItem("style", "sombre");
+    console.log(styleValue);
+  }
+  else if(styleValue == "sombre"){
+    linkElement.setAttribute('href', "style/" + css);
+    localStorage.setItem("style", "normal");
+    console.log(styleValue);
+
+  }
+}
